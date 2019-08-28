b772071ad5d0ba1b10e9db950f97f0024fac9323
egushiken <erick.gushiken@bravium.com.br>
2019-08-27 22:49:22 -0300

    Parte referente ao lab2

--- Exemplo/db/seeds.rb
+++ Exemplo/db/seeds.rb
@@ -1,60 +1,55 @@
 #---
+# Excerpted from "Agile Web Development with Rails 5",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails5 for more book information.
 #---
 # encoding: utf-8
 Product.delete_all
-Product.create!(title: 'Autorretrato e Outras Cronicas',
+Product.create!(title: 'Rails, Angular, Postgres, and Bootstrap',
   description:
     %{<p>
-      <em>Autorretrato e Outras Cronicas</em>
-      Em edicao comemorativa dos 75 anos do Grupo Editorial Record, esta 
-	  coletanea de cronicas de Carlos Drummond de Andrade, com selecao de 
-	  Fernando Py, e relancada com capa nova e encarte contendo fotos e 
-	  uma selecao de correspondencias ineditas do poeta com Alfredo Machado, 
-	  fundador do Grupo. Com o habitual sentimento terno e amargo diante 
-	  dos absurdos e da beleza da vida, Drummond registra, com perfeicao 
-	  e elegancia, fatos da vida diaria e da politica brasileira, a morte 
-	  de amigos, a natureza, a literatura, e ate um bem-humorado e razoavelmente 
-	  condescendente autorretrato: "O sr. Carlos Drummond de Andrade e um razoavel 
-	  prosador que se julga bom poeta, no que se ilude. Como prosador, assinou 
-	  algumas cronicas e alguns contos que revelam certo conhecimento das formas 
-	  graciosas de expressao, certo humour e malicia."
-	  </p>},
+      <em>Powerful, Effective, and Efficient Full-Stack Web Development</em>
+      As a Rails developer, you care about user experience and performance,
+      but you also want simple and maintainable code. Achieve all that by
+      embracing the full stack of web development, from styling with
+      Bootstrap, building an interactive user interface with AngularJS, to
+      storing data quickly and reliably in PostgreSQL. Take a holistic view of
+      full-stack development to create usable, high-performing applications,
+      and learn to use these technologies effectively in a Ruby on Rails
+      environment.
+      </p>},
   image_url: 'Livro1.jpg',    
   price: 45.00)
 # . . .
-Product.create!(title: 'Uma Forma de Saudade. Paginas de Diario',
+Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
   description:
     %{<p>
-      <em>Uma Forma de Saudade. Paginas de Diario</em>
-      Trinta anos apos a morte de Carlos Drummond de Andrade, as paginas arrancadas 
-	  de seu diario e guardadas por sua filha Maria Julieta num envelope com a 
-	  inscricao "Diario de papai/ Familia e amigos" compoem Uma forma de saudade, 
-	  edicao especial que a Companhia das Letras lancara em 31 de outubro deste 
-	  ano, Dia D. Precedidas por uma introducao de Pedro Augusto Grana Drummond, 
-	  organizador do volume, e sucedidas por uma seleta da poemas, as paginas 
-	  revelam ao publico reflexoes do poeta acerca de familiares e amigos proximos 
-	  como Manuel Bandeira e Rodrigo Melo Franco de Andrade. O livro conta ainda 
-	  com fotos do arquivo da familia e fac-similes das anotacoes do poeta, alem 
-	  de projeto grafico especial, assinado por Raul Loureiro.
+      <em>Native Apps, Multiple Platforms</em>
+      Answer the question “Can we build this for ALL the devices?” with a
+      resounding YES. This book will help you get there with a real-world
+      introduction to seven platforms, whether you’re new to mobile or an
+      experienced developer needing to expand your options. Plus, you’ll find
+      out which cross-platform solution makes the most sense for your needs.
       </p>},
   image_url: 'Livro2.jpg',
   price: 26.00)
 # . . .
 
-Product.create!(title: 'Declaracao de Amor',
+Product.create!(title: 'Ruby Performance Optimization',
   description:
     %{<p>
-      <em>Declaracao de Amor</em> 
-      Com o subtitulo "Cancao de namorados", esta reuniao de poemas amorosos, 
-	  romanticos e deliciosamente apaixonados de Carlos Drummond de Andrade 
-	  mostra a faceta mais lirica do grande poeta mineiro. Textos ja classicos 
-	  ou que merecem uma nova leitura, como "Amar", "Lembrete", "Ausencia", 
-	  "Toada do amor", "Declaracao de amor" e "O chao e cama" foram criteriosamente 
-	  selecionados por Luis Mauricio e Pedro Augusto Grana Drummond, netos do poeta 
-	  e grandes conhecedores de sua obra. O resultado e uma celebracao de beijos, 
-	  abracos e carinhos - uma festa para o amor, enfim. Com projeto grafico exclusivo 
-	  e ilustracoes do aclamado artista Nik Neves, Declaracao de amor e o presente 
-	  ideal para o Dia dos Namorados.
+      <em>Why Ruby Is Slow, and How to Fix It</em> 
+      You don’t have to accept slow Ruby or Rails performance. In this
+      comprehensive guide to Ruby optimization, you’ll learn how to write
+      faster Ruby code—but that’s just the beginning. See exactly what makes
+      Ruby and Rails code slow, and how to fix it. Alex Dymo will guide you
+      through perils of memory and CPU optimization, profiling, measuring,
+      performance testing, garbage collection, and tuning. You’ll find that
+      all those “hard” things aren’t so difficult after all, and your code
+      will run orders of magnitude faster.
       </p>},
   image_url: 'Livro3.jpg',
   price: 46.00)
--- Exemplo/test/controllers/products_controller_test.rb
+++ Exemplo/test/controllers/products_controller_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
-	 @update = {
-        title:         'Lorem  Ipsum',
-        description: 'Wibbles  are fun!',
-        image_url:    'lorem.jpg',
-        price:         19.95
-      }
+	@update = { 
+		title: 'Lorem Ipsum',
+		description: 'Wibbles are fun!',
+		image_url: 'lorem.jpg',
+		price: 19.95
+		}
   end
 
   test "should get index" do
@@ -23,7 +23,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-	 post products_url , params: { product: @update }
+      post products_url, params: { product: @update }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -40,7 +40,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-     patch product_url(@product), params: { product: @update}
+    patch product_url(@product), params: { product: @update }
     assert_redirected_to product_url(@product)
   end
 
@@ -50,5 +50,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to products_url
+  end
+  
+  test "can ’t delete product in cart " do
+	assert_difference('Product.count', 0) do
+		delete product_url(products(:two))
+	end
+
+	assert_redirected_to products_url
   end
 end
